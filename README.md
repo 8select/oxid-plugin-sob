bdf0c5292d0d3e24189052c2b4b72be67b88eff5
Markus Schall <schall@a-sign.ch>
2018-06-29 10:31:15 +0200

    Init base module

---
+++ README.md
@@ -0,0 +1,13 @@
+# A-SIGN GmbH 8select (CSA) Module
+
+## Installation
+
+TBD
+
+## Changelog
+
+Please see the [CHANGELOG file](/CHANGELOG.md).
+
+## License
+
+TBD
---
+++ modules/asign/8select/metadata.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.1';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'asign_8select',
+    'title'       => 'A-SIGN GmbH - 8select CSE',
+    'description' => array(
+        'de' => 'Modul für die 8select Curated Shopping Engine (CSE)',
+        'en' => 'Module for 8select Curated Shopping Engine (CSE)',
+    ),
+    'thumbnail'   => 'asign_logo.png',
+    'version'     => '1.0.0',
+    'author'      => 'A-SIGN GmbH',
+    'url'         => 'https://www.a-sign.ch',
+    'email'       => 'info@a-sign.ch',
+    'extend'      => array(),
+    'files'       => array(),
+    'events'      => array(),
+    'templates'   => array(),
+    'blocks'      => array(),
+    'settings'    => array(),
+);
