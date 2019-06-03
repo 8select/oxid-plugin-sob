46088057eee6d9a0e4869120041e565c53a0813a
Maximilian Zwack <mzwack@noriskshop.de>
2019-06-03 16:03:49 +0200

    https://poc.noriskshop.de/browse/EIGHT-8

--- metadata.php
+++ metadata.php
@@ -14,7 +14,7 @@ $aModule = [
                       <p>Gerne begleiten wir Sie bei der Installation und stehen auch sonst für alle Fragen rund um die Installation zur Verfügung. Sie erreichen uns bei Fragen unter <b>+49 (0)941 20 609 6-10</b> und per E-Mail unter <b><a href="mailto:onboarding@8select.de">onboarding@8select.de</a></b></p>
                       <p>Ihr 8select-Team</p>',
     'thumbnail'   => '8selectLogo.jpeg',
-    'version'     => '__VERSION__',
+    'version'     => '2.0.0',
     'author'      => 'A-SIGN GmbH',
     'url'         => 'https://www.a-sign.ch',
     'email'       => 'info@a-sign.ch',
